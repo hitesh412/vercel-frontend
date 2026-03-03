46a1872840fab27230e3092132d12b23cf0ac147
hitesh412 <hiteshsarkale38@gmail.com>
2026-03-03 18:43:08 +0530

    modify profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,57 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react'
 
 const Profile = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const token = localStorage.getItem("token");
-
-        const res = await axios.get(
-          "http://localhost:5000/api/users/profile",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        setData(res.data);
-      } catch (error) {
-        console.log("Error loading profile");
-      }
-    };
-
-    fetchProfile();
-  }, []);
-
-  if (!data) return <h2>Loading...</h2>;
-
   return (
-    <div>
-      <h2>My Profile</h2>
+    <div>Profile</div>
+  )
+}
 
-      <p><strong>Username:</strong> {data.username}</p>
-      <p><strong>Email:</strong> {data.email}</p>
-      <p><strong>Total Orders:</strong> {data.totalOrders}</p>
-
-      <h3>My Orders</h3>
-
-      {data.orders.length === 0 ? (
-        <p>No orders found</p>
-      ) : (
-        data.orders.map((order) => (
-          <div key={order._id}>
-            <p><strong>Total:</strong> ₹{order.total}</p>
-            <p><strong>Date:</strong> {new Date(order.createdAt).toLocaleDateString()}</p>
-            <hr />
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
-
-export default Profile;
+export default Profile
