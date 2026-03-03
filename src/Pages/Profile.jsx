c2f298832c7f8a1dc1c63e43480c61b98f9bb6c5
hitesh412 <hiteshsarkale38@gmail.com>
2026-03-03 17:34:46 +0530

    modify profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,53 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react'
 
 const Profile = () => {
-  const [profile, setProfile] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const token = localStorage.getItem("token");
-
-        if (!token) {
-          setError("Please login first");
-          setLoading(false);
-          return;
-        }
-
-        const res = await axios.get(
-          "https://project-backend-txga.onrender.com/api/profile",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        setProfile(res.data);
-      } catch (err) {
-        setError("Failed to load profile");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProfile();
-  }, []);
-
-  if (loading) return <h2>Loading...</h2>;
-  if (error) return <h2>{error}</h2>;
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>My Profile</h1>
-      <p><strong>Username:</strong> {profile.name}</p>
-      <p><strong>Email:</strong> {profile.email}</p>
-      <p><strong>Total Orders:</strong> {profile.totalOrders}</p>
-    </div>
-  );
-};
+    <div>Profile</div>
+  )
+}
 
-export default Profile;
+export default Profile
